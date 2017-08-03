9634f5a5b7b2f7173232d3efcd88b2796e02e43e
John <jdott@zoll.com>
2017-08-03 15:24:11 -0400

    Add basic event handling

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 import tkinter
 from time import sleep
+		
 
 class TicTacToe(tkinter.Tk):
 	def __init__(self, parent):
@@ -23,6 +24,11 @@ class TicTacToe(tkinter.Tk):
 		board.create_line(0, 100, 300, 100)
 		board.create_line(0, 200, 300, 200)
 		
+		# bind the left mouse click event to the board 
+		board.bind("<Button-1>", self.screen_click)
+		
+		self.game_board = board
+		
 		# display a change-able message
 		display_message = tkinter.StringVar()
 		display_message.set("Let's play Tic-Tac-Toe!")	
@@ -31,8 +37,62 @@ class TicTacToe(tkinter.Tk):
 		
 		# disable resizing
 		self.resizable(False, False)
+	
+	def determine_sector(self, x, y):
+		if x < 100:
+			if y < 100:
+				return 1
+			elif y < 200:
+				return 4
+			else:
+				return 7
+		elif x < 200:
+			if y < 100:
+				return 2
+			elif y < 200:
+				return 5
+			else:
+				return 8
+		else:
+			if y < 100:
+				return 3
+			elif y < 200:
+				return 6
+			else:
+				return 9
 
+	def screen_click(self, event):
+		sector = self.determine_sector(event.x, event.y)
+		set_sector(sector)
+	
+	#def draw_x_in(self, sector)
+	#def draw_o_in(self, sector)
+	
+def set_sector(sector):
+	global current_player, game_tiles
+	sector_index = sector - 1
+	
+	if current_player%2 == 0:
+		to_set = 'x'
+	else:
+		to_set = 'o'
+
+	game_tiles[sector_index] = to_set
+	#TODO draw shape on canvas 
+	current_player += 1
+	check_win_condition()
+
+def check_win_condition():
+	global game_tiles
+	#TODO add win condition check
+	
 if __name__ == '__main__':
+	global current_player
+	current_player = 0
+	
+	global game_tiles
+	game_tiles = [i for i in range(1,10)]
+	
 	my_game = TicTacToe(None)
 	my_game.title("Tic Tac Toe")
 	my_game.mainloop()
