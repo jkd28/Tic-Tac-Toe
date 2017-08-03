94a54891adcf4e0d889fae2c15de2eca81e30eea
John <jdott@zoll.com>
2017-08-03 16:06:49 -0400

    Begin process of adding canvas updates for shapes

--- tictactoe.py
+++ tictactoe.py
@@ -27,8 +27,6 @@ class TicTacToe(tkinter.Tk):
 		# bind the left mouse click event to the board 
 		board.bind("<Button-1>", self.screen_click)
 		
-		self.game_board = board
-		
 		# display a change-able message
 		display_message = tkinter.StringVar()
 		display_message.set("Let's play Tic-Tac-Toe!")	
@@ -60,25 +58,62 @@ class TicTacToe(tkinter.Tk):
 				return 6
 			else:
 				return 9
-
+				
+	def get_sector_tops(self, sector):
+		return {
+			1: {'left_x':0,'left_y':0,  'right_x':100, 'right_y':0},
+			2: {'left_x':100,'left_y':0,  'right_x':200, 'right_y':0},
+			3: {'left_x':200,'left_y':0,  'right_x':300, 'right_y':0},
+			4: {'left_x':0,'left_y':100,  'right_x':100, 'right_y':100},
+			5: {'left_x':100,'left_y':100,  'right_x':200, 'right_y':100},
+			6: {'left_x':200,'left_y':100,  'right_x':300, 'right_y':100},
+			7: {'left_x':0,'left_y':200,  'right_x':100, 'right_y':200},
+			8: {'left_x':100,'left_y':200,  'right_x':200, 'right_y':200},
+			9: {'left_x':200,'left_y':200,  'right_x':300, 'right_y':200}
+		}[sector]
+	 
+	def get_sector_bottoms(self, sector):
+		return {
+			1: {'left_x':0,'left_y':100, 'right_x':100, 'right_y':100},
+			2: {'left_x':100,'left_y':100, 'right_x':200, 'right_y':100},
+			3: {'left_x':200,'left_y':100, 'right_x':300, 'right_y':100},
+			4: {'left_x':0,'left_y':200, 'right_x':100, 'right_y':200} ,
+			5: {'left_x':100,'left_y':200, 'right_x':200, 'right_y':200},
+			6: {'left_x':200,'left_y':200, 'right_x':300, 'right_y':200},
+			7: {'left_x':0,'left_y':300, 'right_x':100, 'right_y':300},
+			8: {'left_x':100,'left_y':300, 'right_x':200, 'right_y':300},
+			9: {'left_x':200,'left_y':300, 'right_x':300, 'right_y':300}
+		}[sector]
+	
+	def draw_x_in(self, sector, event):
+		top = self.get_sector_tops(sector)
+		bottom = self.get_sector_bottoms(sector)
+		print('Drawing line')
+		print('(' + str(top['left_x']) + ', ' + str(top['left_y']) + ') to (' + str(bottom['left_x']) + ', ' + str(bottom['left_y']) + ')')
+		event.widget.create_line(top['left_x'], top['left_y'], bottom['left_x'], bottom['left_y'], fill="black")
+		self.update()
+			
+	def draw_o_in(self, sector, event):
+		#print('Will draw Circle')
+		return 
+		
 	def screen_click(self, event):
 		sector = self.determine_sector(event.x, event.y)
-		set_sector(sector)
+		set_sector(sector, event)
 	
-	#def draw_x_in(self, sector)
-	#def draw_o_in(self, sector)
-	
-def set_sector(sector):
+def set_sector(sector, event):
 	global current_player, game_tiles
 	sector_index = sector - 1
 	
 	if current_player%2 == 0:
 		to_set = 'x'
+		my_game.draw_x_in(sector, event)
 	else:
 		to_set = 'o'
+		my_game.draw_o_in(sector, event)
 
 	game_tiles[sector_index] = to_set
-	#TODO draw shape on canvas 
+	
 	current_player += 1
 	check_win_condition()
 
@@ -93,6 +128,7 @@ if __name__ == '__main__':
 	global game_tiles
 	game_tiles = [i for i in range(1,10)]
 	
+	global my_game
 	my_game = TicTacToe(None)
 	my_game.title("Tic Tac Toe")
 	my_game.mainloop()
